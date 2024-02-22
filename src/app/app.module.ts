e4073501c597528198300772796557b3f1ffc6e5
Jonas <jjosinoms@gmail.com>
2024-02-22 17:04:18 +0000

    Front Semi Pronto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RecuperarSenhaComponent } from './components/recuperar-senha/recuperar-senha.component';
+import { LoginComponent } from './components/login/login.component';
+import { CriarContaComponent } from './components/criar-conta/criar-conta.component';
+
+// mapeamento das rotas do projeto
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login'},
+  { path: 'login', component: LoginComponent},
+  { path: 'criar-conta', component: CriarContaComponent},
+  { path: 'recuperar-senha', component: RecuperarSenhaComponent},
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RecuperarSenhaComponent,
+    LoginComponent,
+    CriarContaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/components/criar-conta/criar-conta.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-criar-conta',
+  templateUrl: './criar-conta.component.html',
+  styleUrl: './criar-conta.component.css'
+})
+export class CriarContaComponent {
+
+  //atributo
+  apiUrl: string = "http://localhost:3000"
+
+  // construtor para injecao de dependencia
+  constructor(
+    private HttpClient: HttpClient //inicializacao automatica
+    ){
+  }
+
+  // objeto para capturar o formulário
+  formCriarConta = new FormGroup({
+    nome: new FormControl('', 
+      [Validators.required, Validators.minLength(8), Validators.maxLength(150)]),
+    grupo: new FormControl('', 
+    [Validators.required, Validators.minLength(1), Validators.maxLength(2)]),
+    /*
+    email: new FormControl('',
+      [Validators.required, Validators.email]),
+    senha: new FormControl('',
+      [Validators.required, Validators.minLength(8), Validators.maxLength(20)])
+      */
+  })
+
+  // onjeto para executar as validações
+
+  get form(): any{
+    return this.formCriarConta.controls;
+  }
+
+  // funcao para capturar um SUBMIT do formulario
+
+  onSubmit(){
+    
+    //fazendo uma requisicao POST para o endpoint /api/selecoes
+    this.HttpClient.post(this.apiUrl + "/selecoes", this.formCriarConta.value)
+    .subscribe(/*capturar retorno da api*/ {
+      next: (data) => { //resposta de sucesso da API
+        console.log(data)
+      },
+      error: (e)=> { //resposta de erro da API
+        console.log(e.error)
+      }
+    })
+
+  }
+}
